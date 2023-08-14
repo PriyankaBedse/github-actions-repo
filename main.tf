abe722e88a7c4fb1fed3920842efadad7c014206
PriyankaBedse <88850595+PriyankaBedse@users.noreply.github.com>
2023-08-14 15:35:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "ASIA2PKU5HI7L7UAAWH5"
-  secret_key = "MKKC0y50mw1KvFtMAmTrTs/AngPUEhgEybPRSvL4"
+
 }
 resource "aws_s3_bucket" "bucket" {
   bucket = "test-github-action"
